d1ab59a62611830b5168109b7f26274727c17608
Daniel Bretoi <daniel@netwalk.org>
2011-05-04 16:16:45 -0700

    skeleton functions

---
+++ lib/adapter/git.rb
@@ -0,0 +1,10 @@
+module Scoop
+  module Adapter
+    class Git
+      def local_revision
+      end
+      def remote_revision
+      end
+    end
+  end
+end
--- lib/scoop.rb
+++ lib/scoop.rb
@@ -1,13 +1,15 @@
 module Scoop
   # Your code goes here...
   class Builder
-    def root
-      'foo'
-    end
     def config
       YAML.load_file((Scoop.root + 'config/config.yml').to_s)
       root = Scoop.root
       YAML::load(ERB.new( IO.read( (Scoop.root+'config/config.yml').to_s ) ).result(binding) )
+    end
+
+    def run
+      run_build_tasks
+      run_deploy_tasks
     end
   end
 
