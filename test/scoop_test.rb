258e6f394e27ed8415449668cdbcdf58b33560b4
Daniel Bretoi <daniel@netwalk.org>
2011-05-16 19:51:38 -0700

    fix test

--- test/scoop_test.rb
+++ test/scoop_test.rb
@@ -2,7 +2,7 @@ require './test/helper'
 
 describe Scoop do
   it "load config correct" do
-    Scoop.config[:source_dir].must_equal File.join(File.dirname(File.realpath(__FILE__)),'src')
+    Scoop::Builder.new.config[:source_dir].must_equal File.join(File.dirname(File.realpath(__FILE__)),'src')
   end
   it "should run build tasks correct" do
   end
