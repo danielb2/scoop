03ce681e0d7181a992d40f5954287bfdd1504279
Daniel Bretoi <nefar@otherware.org>
2011-08-07 23:19:23 -0700

    add test for shell command

--- spec/common_spec.rb
+++ spec/common_spec.rb
@@ -1,7 +1,14 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Scoop do
+describe Scoop::Common do
+  include Scoop::Common
   before do
   end
-  it "should output stderr fine when using shell command"
+  it "should output stderr fine when using shell command" do
+    begin
+      shell("missing scoop").should == 'blah'
+    rescue Exception => e
+      e.output.should == "sh: missing: command not found\n"
+    end
+  end
 end
